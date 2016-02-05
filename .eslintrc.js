b3a2a603b5f9975962a2e9049fccfdf4bc3a9df5
cxg8590 <cxg8590@rit.edu>
2016-02-05 01:23:08 -0500

    added eslint

---
+++ .eslintrc.js
@@ -0,0 +1,25 @@
+module.exports = {
+    "rules": {
+        "indent": [
+            2,2
+        ],
+        "no-console": [0],
+        "no-undef":[0],
+        "quotes": [
+            2,
+            "single"
+        ],
+        "linebreak-style": [
+            2,
+            "windows"
+        ],
+        "semi": [
+            0,
+            "never"
+        ]
+    },
+    "env": {
+        "es6": true,
+        "browser": false
+    },
+};
